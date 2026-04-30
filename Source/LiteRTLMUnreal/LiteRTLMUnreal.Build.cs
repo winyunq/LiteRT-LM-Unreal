607bc5b5db3cc24b837dd6166311d37f6ffa3ddd
winyunq <winyunq@qq.com>
2026-04-30 14:02:14 +0800

    Restructure: Move ThirdParty into LiteRTLMUnreal module and update Build.cs

--- Source/LiteRTLMUnreal/LiteRTLMUnreal.Build.cs
+++ Source/LiteRTLMUnreal/LiteRTLMUnreal.Build.cs
@@ -10,7 +10,7 @@ public class LiteRTLMUnreal : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "Projects" });
 
 		// ThirdParty Path
-		string ThirdPartyPath = Path.GetFullPath(Path.Combine(ModuleDirectory, "../../ThirdParty/LiteRtLm"));
+		string ThirdPartyPath = Path.GetFullPath(Path.Combine(ModuleDirectory, "ThirdParty/LiteRtLm"));
 		PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "Include"));
 
 		if (Target.Platform == UnrealTargetPlatform.Win64)
